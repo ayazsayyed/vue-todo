bf90218eee93aa3ea2853195fd99afa91cc89e43
Aayaz <ayaz.sayyed94@gmail.com>
2019-06-21 21:55:15 +0530

    Priority, tags, description etc

--- src/components/TodoDetailModal.vue
+++ src/components/TodoDetailModal.vue
@@ -25,19 +25,256 @@
                   <i class="fa fa-edit" aria-hidden="true"></i>
                 </div>
               </div>
+              <div class="task-desc task-desc mt-3 text-right">
+                <p class="text-primary add-link" @click="toggleAddDecriptionField">Add description</p>
+                <textarea
+                  v-if="showAddDecriptionField"
+                  class="form-control"
+                  rows="3"
+                  placeholder="Write a dewscription text here ..."
+                ></textarea>
+              </div>
             </div>
             <div class="task-features">
-              <div class="task-calender">
+              <div class="nav-wrapper">
+                <ul
+                  class="nav nav-pills nav-fill flex-column flex-md-row"
+                  id="tabs-icons-text"
+                  role="tablist"
+                >
+                  <li class="nav-item">
+                    <a
+                      class="nav-link mb-sm-3 mb-md-0"
+                      id="tabs-icons-text-1-tab"
+                      data-toggle="tab"
+                      href="#tabs-icons-text-1"
+                      role="tab"
+                      aria-controls="tabs-icons-text-1"
+                      aria-selected="true"
+                    >
+                      <i class="fa fa-calendar mr-2" aria-hidden="true"></i>Calendar
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link mb-sm-3 mb-md-0"
+                      id="tabs-icons-text-2-tab"
+                      data-toggle="tab"
+                      href="#tabs-icons-text-2"
+                      role="tab"
+                      aria-controls="tabs-icons-text-2"
+                      aria-selected="false"
+                    >
+                      <i class="fa fa-exclamation" aria-hidden="true"></i>
+                      <i class="fa fa-exclamation" aria-hidden="true"></i>
+                      <i class="fa fa-exclamation mr-2" aria-hidden="true"></i>Priority
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a
+                      class="nav-link mb-sm-3 mb-md-0"
+                      id="tabs-icons-text-3-tab"
+                      data-toggle="tab"
+                      href="#tabs-icons-text-3"
+                      role="tab"
+                      aria-controls="tabs-icons-text-3"
+                      aria-selected="false"
+                    >
+                      <i class="fa fa-tags mr-2" aria-hidden="true"></i>Tags
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="tab-content" id="myTabContent">
+                <div
+                  class="tab-pane fade task-calendar"
+                  id="tabs-icons-text-1"
+                  role="tabpanel"
+                  aria-labelledby="tabs-icons-text-1-tab"
+                >
+                  <div class="card shadow">
+                    <div class="card-body">
+                      <p class="description">Schedule your task by adding date and time</p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="tab-pane fade task-priority"
+                  id="tabs-icons-text-2"
+                  role="tabpanel"
+                  aria-labelledby="tabs-icons-text-2-tab"
+                >
+                  <div class="card shadow">
+                    <div class="card-body">
+                      <p class="description">Set priority of your task</p>
+                      <div class="custom-control custom-radio mb-2">
+                        <input
+                          name="custom-radio-1"
+                          class="custom-control-input"
+                          id="customRadio1"
+                          type="radio"
+                        >
+                        <label class="custom-control-label high" for="customRadio1">
+                          <div class="icon-wrapper">
+                            <i class="fa fa-exclamation" aria-hidden="true"></i>
+                            <i class="fa fa-exclamation" aria-hidden="true"></i>
+                            <i class="fa fa-exclamation" aria-hidden="true"></i>
+                          </div>High
+                        </label>
+                      </div>
+                      <div class="custom-control custom-radio mb-2">
+                        <input
+                          name="custom-radio-1"
+                          class="custom-control-input"
+                          id="customRadio2"
+                          type="radio"
+                        >
+                        <label class="custom-control-label medium" for="customRadio2">
+                          <div class="icon-wrapper">
+                            <i class="fa fa-exclamation" aria-hidden="true"></i>
+                            <i class="fa fa-exclamation" aria-hidden="true"></i>
+                          </div>Medium
+                        </label>
+                      </div>
+                      <div class="custom-control custom-radio mb-2">
+                        <input
+                          name="custom-radio-1"
+                          class="custom-control-input"
+                          id="customRadio3"
+                          type="radio"
+                        >
+                        <label class="custom-control-label low" for="customRadio3">
+                          <div class="icon-wrapper">
+                            <i class="fa fa-exclamation" aria-hidden="true"></i>
+                          </div>Low
+                        </label>
+                      </div>
+                      <div class="custom-control custom-radio mb-2">
+                        <input
+                          name="custom-radio-1"
+                          class="custom-control-input"
+                          id="customRadio4"
+                          type="radio"
+                        >
+                        <label class="custom-control-label none" for="customRadio4">
+                          <div class="icon-wrapper">
+                            <i class="fa fa-exclamation" aria-hidden="true"></i>
+                            <i class="fa fa-exclamation" aria-hidden="true"></i>
+                            <i class="fa fa-exclamation" aria-hidden="true"></i>
+                          </div>None
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="tab-pane fade task-tags"
+                  id="tabs-icons-text-3"
+                  role="tabpanel"
+                  aria-labelledby="tabs-icons-text-3-tab"
+                >
+                  <div class="card shadow">
+                    <div class="card-body">
+                      <div class="new-tag">
+                        <div class="row">
+                          <div class="col-md-12">
+                            <div class="form-group">
+                              <input
+                                type="text"
+                                class="form-control form-control-alternative"
+                                id="exampleFormControlInput1"
+                                placeholder="Enter tag"
+                                autocomplete="off"
+                              >
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <ul class="tags-wrapper">
+                        <li class="tag-item">
+                          <div class="color-option-icon">
+                            <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                          </div>
+                          <div class="tag-icon">
+                            <i class="fa fa-tags" aria-hidden="true"></i>
+                          </div>
+                          <div class="tag-name">
+                            <p>
+                              <strong>Work</strong>
+                            </p>
+                          </div>
+                          <div class="action">
+                            <div class="custom-control custom-checkbox mb-3">
+                              <input class="custom-control-input" id="customCheck1" type="checkbox">
+                              <label class="custom-control-label" for="customCheck1">
+                                <span></span>
+                              </label>
+                            </div>
+                          </div>
+                        </li>
+                        <li class="tag-item">
+                          <div class="color-option-icon">
+                            <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                          </div>
+                          <div class="tag-icon">
+                            <i class="fa fa-tags" aria-hidden="true"></i>
+                          </div>
+                          <div class="tag-name">
+                            <p>
+                              <strong>Home</strong>
+                            </p>
+                          </div>
+                          <div class="action">
+                            <div class="custom-control custom-checkbox mb-3">
+                              <input class="custom-control-input" id="customCheck2" type="checkbox">
+                              <label class="custom-control-label" for="customCheck2">
+                                <span></span>
+                              </label>
+                            </div>
+                          </div>
+                        </li>
+                        <li class="tag-item">
+                          <div class="color-option-icon">
+                            <i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+                          </div>
+                          <div class="tag-icon">
+                            <i class="fa fa-tags" aria-hidden="true"></i>
+                          </div>
+                          <div class="tag-name">
+                            <p>
+                              <strong>Personal</strong>
+                            </p>
+                          </div>
+                          <div class="action">
+                            <div class="custom-control custom-checkbox mb-3">
+                              <input class="custom-control-input" id="customCheck3" type="checkbox">
+                              <label class="custom-control-label" for="customCheck3">
+                                <span></span>
+                              </label>
+                            </div>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <!-- <div class="task-calender">
                 <i class="fa fa-calendar datepicker" aria-hidden="true"></i>
               </div>
               <div class="task-priority">
                 <i class="fa fa-exclamation" aria-hidden="true"></i>
                 <i class="fa fa-exclamation" aria-hidden="true"></i>
                 <i class="fa fa-exclamation" aria-hidden="true"></i>
+                <div class="task-priority-dropdown dropdown-menu show">
+                    <p>hello world</p>
+                </div>
               </div>
               <div class="task-tasgs">
                 <i class="fa fa-tags" aria-hidden="true"></i>
-              </div>
+              </div>-->
             </div>
           </div>
 
@@ -57,13 +294,17 @@ export default {
   name: "todoDetailModal",
   data() {
     return {
-      taskDetail: {}
+      taskDetail: {},
+      showAddDecriptionField: false
     };
   },
   components: {
     Bus
   },
   methods: {
+    toggleAddDecriptionField() {
+      this.showAddDecriptionField = !this.showAddDecriptionField;
+    },
     showModal(data) {
       this.taskDetail = data;
       $("#genericPopup").modal("show");
@@ -76,11 +317,8 @@ export default {
 </script>
 
 <style scoped lang="scss" >
-.modal-body {
-  padding-bottom: 0;
-}
 .content-wrapper {
-  height: 200px;
+  //   height: 200px;
 }
 .task-wrapper {
   display: flex;
@@ -96,27 +334,83 @@ export default {
     }
   }
 }
-.task-features {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  padding: 10px 0;
-  border-top: 1px solid #ced3d4;
-  i {
-    cursor: pointer;
-    font-size: 18px;
-  }
-  .fa.fa-exclamation {
-    letter-spacing: -3px;
-  }
-  & > *{
-      flex-grow: 1;
+.custom-control {
+  .icon-wrapper {
+    width: 40px;
+    display: inline-block;
     text-align: center;
-    border-right: 1px solid #ced3d4;
-    &:last-child{
-    border-right: 0;   
+  }
+  .high i {
+    color: #f5365c;
+  }
+  .medium i {
+    color: #ffbb33;
+  }
+  .low i {
+    color: #5e72e4;
+  }
+  .none i {
+    color: #37474f;
+  }
+}
+.task-desc {
+  p.add-link {
+    font-size: 15px;
+    text-decoration: underline;
+    font-weight: 500;
+    display: inline;
+    cursor: pointer;
+  }
+}
+// #tabs-icons-text-3 {
+//   .card-body {
+//     padding: 1.7rem;
+//   }
+// }
+ul.tags-wrapper {
+  padding-left: 0;
+  li.tag-item {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-height: 30px;
+    position: relative;
+    padding-left: 25px;
+    cursor: default;
+    &:hover {
+      .color-option-icon {
+        display: block;
+      }
+    }
+    .color-option-icon {
+      position: absolute;
+      left: 8px;
+      display: none;
+      cursor: pointer;
+    }
+    .tag-icon {
+      margin-right: 10px;
+    }
+    .tag-name {
+      flex: 1;
+      p {
+        margin: 0;
+      }
+    }
+    .action {
+      .custom-checkbox {
+        padding: 0;
+        margin: 0;
+        .custom-control-label::before,
+        .custom-control-label::after {
+          top: -10px;
+        }
+        span {
+          display: none;
+        }
+      }
     }
   }
 }
-
 </style>
