befcacb7008a8fc7b21fe4126a090c504a4cf447
Ayaz Sayyed <ayaz.sayyed94@gmail.com>
2023-06-13 15:35:55 +0530

    [vue-3-migration] -> App setup | Initial migration

--- src/main.js
+++ src/main.js
@@ -1,46 +1,11 @@
-import Vue from 'vue'
+import './assets/main.css'
+import 'mdb-vue-ui-kit/css/mdb.min.css';
+import { createApp } from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import firebase from 'firebase'
-import Notifications from 'vue-notification'
+import router from './router'
 
-import login from './components/Login.vue'
-import PageNotFound from './components/Page404.vue'
-import todo from './components/Todo.vue'
-import register from './components/Register.vue'
-import store from './components/store/index'
+const app = createApp(App)
 
+app.use(router)
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAjBiYXlMX5w-AoXmc2FTi7sfgHr8He7S8",
-  authDomain: "vue-todo-1.firebaseapp.com",
-  databaseURL: "https://vue-todo-1.firebaseio.com",
-  projectId: "vue-todo-1",
-  storageBucket: "vue-todo-1.appspot.com",
-  messagingSenderId: "203196754559",
-  appId: "1:203196754559:web:6dd477dc3577b801"
-};
-
-firebase.initializeApp(firebaseConfig)
-Vue.config.productionTip = false
-Vue.use(Notifications)
-Vue.use(VueRouter);
-
-const routes = [
-  { path:  '/', component: login },
-  { path:  '/login', component: login },
-  { path: '/todo', component: todo },
-  { path: "*", component: PageNotFound},
-  { path: '/register', component: register },
-];
-
-const router = new VueRouter({
-  routes,
-  mode:'history'
-})
-new Vue({
-  router,
-  store,
-  el: '#app',
-  render: h => h(App)
-})
+app.mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Login.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/Register.vue')
+    }
+  ]
+})
+
+export default router
