5b2740152e9d7f119205b54ed914eb26cab3493e
Ayaz Sayyed <ayaz.sayyed94@gmail.com>
2023-06-17 00:45:18 +0530

    [vue-3-migration] -> Vue-3 app setup | Initial migration

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
